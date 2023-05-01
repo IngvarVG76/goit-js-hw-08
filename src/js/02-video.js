600b8765a07764651da02b3ec295184cc0df51db
Igor Grybanov <igor.gribanov@gmail.com>
2023-05-01 20:10:48 +0300

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const THROTTLE_TIME = 1000; 
+const storageKey = 'vimeo-paused-time';
+
+player.on(
+  'timeupdate',
+  throttle(() => {
+    player.getCurrentTime().then(currentTime => {
+      localStorage.setItem(storageKey, currentTime);
+    });
+  }, THROTTLE_TIME)
+);
+
+const savedTime = localStorage.getItem(storageKey);
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
